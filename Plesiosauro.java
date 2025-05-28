36957d446da0eec621bdb2ef027ebc50aea233df
ThisAsh <111087776+ThisAsh@users.noreply.github.com>
2025-05-28 20:49:12 +0200

    Add files via upload

---
+++ Plesiosauro.java
@@ -0,0 +1,23 @@
+public class Plesiosauro extends Dinosauro{
+
+    private double dimensione;
+
+    // Costruttore
+    public Plesiosauro(double dimensione) 
+    {
+        this.specie = "Plesiosauro";
+        this.dimensione = dimensione;
+    }
+
+       @Override
+    void descriviRazza()
+    {
+        //chi sei ?
+    }
+
+    @Override
+    void feed() 
+    {
+        //mangia bastardo
+    }
+}
---
+++ Pterodattilo.java
@@ -0,0 +1,23 @@
+public class Pterodattilo {
+
+    private int aperturaAlare;
+
+    // Costruttore
+    public Pterodattilo(int aperturaAlare) 
+    {
+        this.specie = "Pterodattilo";
+        this.aperturaAlare = aperturaAlare;
+    }
+
+       @Override
+    void descriviRazza()
+    {
+        //chi sei ?
+    }
+
+    @Override
+    void feed() 
+    {
+        //mangia bastardo
+    }
+}
